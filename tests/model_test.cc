db343e9df4db3edb41716624c609b3f61b609f1e
Jeffrey Salmond <js947@cam.ac.uk>
2017-04-18 10:28:43 +0100

    :white_check_mark: add test for lattice weights sum to 1

--- tests/model_test.cc
+++ tests/model_test.cc
@@ -27,6 +27,16 @@ TEST_CASE("Model weights are checked", "[LBM][Model]") {
     REQUIRE(d2q9.weight(-1,-1) == Approx(1.0/36.0));
   }
 
+  SECTION("D2Q9 model weights sum to 1") {
+    const auto d2q9 = lbmdem::Model<2, 9>();
+
+    lbmdem::Real sum = 0;
+    for (int i = -1; i <= 1; ++i)
+      for (int j = -1; j <= 1; ++j)
+        sum += d2q9.weight(i,j);
+    REQUIRE(sum == Approx(1.0));
+  }
+
   SECTION("D3Q27 model weights are checked") {
     const auto d3q27 = lbmdem::Model<3, 27>();
 
@@ -58,5 +68,16 @@ TEST_CASE("Model weights are checked", "[LBM][Model]") {
     REQUIRE(d3q27.weight(-1,-1,-1) == Approx(1.0/216.0));
     REQUIRE(d3q27.weight(-1,-1,-1) == Approx(1.0/216.0));
     REQUIRE(d3q27.weight(-1,-1,-1) == Approx(1.0/216.0));
+  }
+
+  SECTION("D3Q27 model weights sum to 1") {
+    const auto d3q27 = lbmdem::Model<3, 27>();
+
+    lbmdem::Real sum = 0;
+    for (int i = -1; i <= 1; ++i)
+      for (int j = -1; j <= 1; ++j)
+        for (int k = -1; k <= 1; ++k)
+          sum += d3q27.weight(i,j,k);
+    REQUIRE(sum == Approx(1.0));
   }
 }
