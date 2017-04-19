ecdd1a908b7cf3836a750ace0a20511730ff70af
Jeffrey Salmond <js947@cam.ac.uk>
2017-04-19 14:58:34 +0100

    make lattice weights and velocities static

--- include/lattice.h
+++ include/lattice.h
@@ -12,45 +12,63 @@ template <unsigned Tdim, unsigned Tq>
 class Lattice;
 }
 
-//! D2Q9 Lattice
-//! \brief Weights for the D2Q9 stencil
-template <>
-class lbmdem::Lattice<2, 9> {
- public:
-  lbmdem::Real weight(int x, int y) const {
-    if (abs(x) > 1) return 0.0;
-    if (abs(y) > 1) return 0.0;
-    switch ((x == 0) + (y == 0)) {
-      case 2:
-        return 4.0 / 9.0;
-      case 1:
-        return 1.0 / 9.0;
-      default:
-        return 1.0 / 36.0;
-    }
+template <unsigned Tdim, unsigned Tq>
+class lbmdem::Lattice {
+  public:
+
+  static const std::array<lbmdem::Real,Tq> weights;
+  static const std::array<std::array<int,Tdim>,Tq> velocities;
+
+  template <typename... Ix>
+  lbmdem::Real weight(Ix... i) const {
+    std::array<int,Tdim> ix{i...};
+    for (int j = 0; j < Tq; ++j)
+      if (ix == velocities.at(j))
+        return weights.at(j);
+    return 0.0;
   }
 };
 
-//! D3Q27 Lattice
-//! \brief Weights for the D3Q27 lattice
 template <>
-class lbmdem::Lattice<3, 27> {
- public:
-  lbmdem::Real weight(int x, int y, int z) const {
-    if (abs(x) > 1) return 0.0;
-    if (abs(y) > 1) return 0.0;
-    if (abs(z) > 1) return 0.0;
-    switch ((x == 0) + (y == 0) + (z == 0)) {
-      case 3:
-        return 8.0 / 27.0;
-      case 2:
-        return 2.0 / 27.0;
-      case 1:
-        return 1.0 / 54.0;
-      default:
-        return 1.0 / 216.0;
-    }
-  }
+const std::array<lbmdem::Real,9> lbmdem::Lattice<2,9>::weights = {
+  1./36., 1./ 9., 1./36.,
+  1./ 9., 4./ 9., 1./ 9.,
+  1./36., 1./ 9., 1./36.,
+};
+template <>
+const std::array<std::array<int,2>,9> lbmdem::Lattice<2,9>::velocities = { std::array<int,2>
+  {-1, 1}, { 0, 1}, { 1, 1},
+  {-1, 0}, { 0, 0}, { 1, 0},
+  {-1,-1}, { 0,-1}, { 1,-1},
+};
+
+template <>
+const std::array<lbmdem::Real,27> lbmdem::Lattice<3,27>::weights = {
+  1./216., 1./ 54., 1./216.,
+  1./ 54., 2./ 27., 1./ 54.,
+  1./216., 1./ 54., 1./216.,
+
+  1./ 54., 2./ 27., 1./ 54.,
+  2./ 27., 8./ 27., 2./ 27.,
+  1./ 54., 2./ 27., 1./ 54.,
+
+  1./216., 1./ 54., 1./216.,
+  1./ 54., 2./ 27., 1./ 54.,
+  1./216., 1./ 54., 1./216.,
+};
+template <>
+const std::array<std::array<int,3>,27> lbmdem::Lattice<3,27>::velocities = { std::array<int,3>
+  {-1, 1, 1}, { 0, 1, 1}, { 1, 1, 1},
+  {-1, 0, 1}, { 0, 0, 1}, { 1, 0, 1},
+  {-1,-1, 1}, { 0,-1, 1}, { 1,-1, 1},
+
+  {-1, 1, 0}, { 0, 1, 0}, { 1, 1, 0},
+  {-1, 0, 0}, { 0, 0, 0}, { 1, 0, 0},
+  {-1,-1, 0}, { 0,-1, 0}, { 1,-1, 0},
+
+  {-1, 1,-1}, { 0, 1,-1}, { 1, 1,-1},
+  {-1, 0,-1}, { 0, 0,-1}, { 1, 0,-1},
+  {-1,-1,-1}, { 0,-1,-1}, { 1,-1,-1},
 };
 
 #endif //LBMDEM_LATTICE_H_
