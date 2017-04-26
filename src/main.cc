73c3cc4a775158d0a0832f64675d5e648fb27f79
Jeffrey Salmond <js947@cam.ac.uk>
2017-04-26 15:59:58 +0100

    fix up the (fake) main

--- src/main.cc
+++ src/main.cc
@@ -4,11 +4,9 @@
 #include "lattice.h"
 #include "factory.h"
 #include "settings.h"
+#include "patch.h"
 
 int main(int argc, char** argv) {
-  lbmdem::Lint id = 0;
   const unsigned Dim = 3;
-  std::array<lbmdem::Lint, Dim> coord = {{0}};
-  auto node = std::make_shared<lbmdem::LBNode<Dim>>(id, coord);
-  std::cout << node->isfluid() << '\n';
+  auto patch = lbmdem::Patch<3,27>();
 }
