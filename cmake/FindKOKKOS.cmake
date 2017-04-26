c383b5e116fc8ff76e243379f8f7231ef7af1eae
Jeffrey Salmond <js947@cam.ac.uk>
2017-04-26 12:41:35 +0100

    :construction: make use of kokkos.cmake in kokkos 2.03.00

--- cmake/FindKOKKOS.cmake
+++ cmake/FindKOKKOS.cmake
@@ -9,16 +9,15 @@ if(NOT ${KOKKOS_ROOT})
 	set(KOKKOS_ROOT "/usr" CACHE PATH "Root directory of Kokkos installation")
 endif()
 
-find_path(KOKKOS_INCLUDE_DIR Kokkos_Core.hpp
-	HINTS ${KOKKOS_ROOT}/inlcude
-   PATHS ${KOKKOS_ROOT}/include)
-find_library(KOKKOS_CORE_LIBRARY NAMES kokkos
-	PATHS ${KOKKOS_ROOT}/lib ${KOKKOS_ROOT}/lib64)
+find_file(KOKKOS_CMAKE "kokkos.cmake" HINTS ${KOKKOS_ROOT} PATHS ENV CMAKE_PREFIX_PATH)
+include(${KOKKOS_CMAKE})
 
-set(KOKKOS_LIBRARIES "${KOKKOS_CORE_LIBRARY}")
+find_path(KOKKOS_INCLUDE_DIR Kokkos_Core.hpp HINTS ${KOKKOS_ROOT}/include)
+
+set(KOKKOS_LIBRARIES "${KOKKOS_LDFLAGS} ${KOKKOS_LIBS}")
 set(KOKKOS_INCLUDE_DIRS "${KOKKOS_INCLUDE_DIR}")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(KOKKOS DEFAULT_MSG KOKKOS_CORE_LIBRARY KOKKOS_INCLUDE_DIR)
+find_package_handle_standard_args(KOKKOS DEFAULT_MSG KOKKOS_CMAKE KOKKOS_LIBRARIES KOKKOS_INCLUDE_DIRS)
 
-mark_as_advanced(KOKKOS_INCLUDE_DIR KOKKOS_CORE_LIBRARY)
+mark_as_advanced(KOKKOS_INCLUDE_DIR KOKKOS_CMAKE)
