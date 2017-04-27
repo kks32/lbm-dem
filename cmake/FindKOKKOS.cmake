fa80c50948b81bbd673520aa5792865b02377851
Krishna Kumar <kks32@users.noreply.github.com>
2017-04-27 14:33:03 +0100

    Kokkos with cmake support (#24)
    * :fire: :construction: Kokkos CMAKE
    
    * :construction: make use of kokkos.cmake in kokkos 2.03.00
    
    * :construction: :dart: CircleCI and KOKKOS with CMake support
    
    * :wrench: Fix Travis KOKKOS
    
    * :nut_and_bolt: Kokkos in /usr
    
    * :nut_and_bolt: Kokkos in /home/krishna
    
    * :hammer: Kokkos in /home/krishna and cd to git repo
    
    * :dart: Travis-CI clang-openmp
    
    * :fire: Removed clang from Travis-CI
    
    * :dart: :computer: Clang3.9 on CircleCI
    
    * :hammer: Kokkos CMake support
    
    * :fire: Remove OpenMP_CXX_FLAGS from CMakeLists

---
+++ cmake/FindKOKKOS.cmake
@@ -0,0 +1,23 @@
+# - try to find KOKKOS
+#
+#  KOKKOS_INCLUDE_DIRS - where to find kokkos.h, etc
+#  KOKKOS_LIBRARIES    - List of libraries when using kokkos.
+#  KOKKOS_FOUND        - True if kokkos found.
+#
+
+if(NOT ${KOKKOS_ROOT})
+	set(KOKKOS_ROOT "/opt/kokkos" CACHE PATH "Root directory of Kokkos installation")
+endif()
+
+find_file(KOKKOS_CMAKE "kokkos.cmake" HINTS ${KOKKOS_ROOT} PATHS ENV CMAKE_PREFIX_PATH)
+include(${KOKKOS_CMAKE})
+
+find_path(KOKKOS_INCLUDE_DIR Kokkos_Core.hpp HINTS ${KOKKOS_ROOT}/include)
+
+set(KOKKOS_LIBRARIES "${KOKKOS_LDFLAGS} ${KOKKOS_LIBS}")
+set(KOKKOS_INCLUDE_DIRS "${KOKKOS_INCLUDE_DIR}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(KOKKOS DEFAULT_MSG KOKKOS_CMAKE KOKKOS_LIBRARIES KOKKOS_INCLUDE_DIRS)
+
+mark_as_advanced(KOKKOS_INCLUDE_DIR KOKKOS_CMAKE)
